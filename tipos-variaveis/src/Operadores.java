2f87cface911cae25d7ce3e3e99819de65c88fca
Jonathan Franks Oliveira <jonathan.franks011@gmail.com>
2023-02-01 17:01:28 -0300

    Inserção da classe OPERADORES

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,20 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+        //Operadores matemáticos
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20 / 4);
+
+        //Usando operadores para junção em string
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+        System.out.println(nomeCompleto);
+
+        //Concatenação
+        
+
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+
+        //Texto
+        String meuNome = "Jonathan Franks Oliveira";
+
+        //Valor real
+        double salarioMinino = 2500.33;
+
+        //Valor inteiro
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+
+        //O 'final' define a variáve como uma constante onde não permite alteração do valor (sempre em caixa alta)
+        final double VALOR_DE_PI = 3.14;
     }
 }
