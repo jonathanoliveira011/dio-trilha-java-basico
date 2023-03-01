6a93f7612197f1a22df08fca1ab8ea85716276b8
Jonathan Franks Oliveira <jonathan.franks011@gmail.com>
2023-03-01 16:01:07 -0300

    #

---
+++ Programação Orientada a Objetos com Java/exercicio-classes/src/Carro.java
@@ -0,0 +1,33 @@
+class Carro {
+
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+void setCor(String cor){
+
+    this.cor = cor;
+
+}
+
+String getCor(){
+
+    return cor;
+
+}
+
+void setModelo(String modelo){
+
+    this.modelo = modelo;
+
+}
+
+String getModelo(){
+
+    return modelo;
+
+}
+
+
+
+}
