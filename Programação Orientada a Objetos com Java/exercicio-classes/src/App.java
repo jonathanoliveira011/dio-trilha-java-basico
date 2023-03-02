6c6ba2d6558e0194789678ef1a55b2d88f1d1605
Jonathan Franks Oliveira <jonathan.franks011@gmail.com>
2023-03-02 09:43:53 -0300

    Conceito de Classes, atributos, métodos e objeto

--- Programação Orientada a Objetos com Java/exercicio-classes/src/App.java
+++ Programação Orientada a Objetos com Java/exercicio-classes/src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //Criando objeto para acessar a Classe
+        Carro car = new Carro();
+        car.setCor("vermelho");
+        car.setModelo("Uno Mille");
+        car.setCapacidadeTanque(59);
+
+        System.out.println("Modelo: "+ car.getModelo());
+        System.out.println("Cor: "+ car.getCor());
+        System.out.println("Capacidade do tanque: "+ car.getCapacidadeTanque());
+        System.out.println("O valor total para abastecimento do tanque é de R$"+ car.calcularValor(5.19));
+
+        //Informando valores direto no objeto
+        Carro carro2 = new Carro("Cinza", "Nivus", 47);
+        System.out.println("Modelo: "+ car.getModelo());
+        System.out.println("Cor: "+ car.getCor());
+        System.out.println("Capacidade do tanque: "+ car.getCapacidadeTanque());
+    
     }
 }
--- Programação Orientada a Objetos com Java/exercicio-classes/src/Carro.java
+++ Programação Orientada a Objetos com Java/exercicio-classes/src/Carro.java
@@ -4,30 +4,69 @@ class Carro {
     String modelo;
     int capacidadeTanque;
 
-void setCor(String cor){
-
-    this.cor = cor;
-
-}
-
-String getCor(){
-
-    return cor;
-
-}
-
-void setModelo(String modelo){
-
-    this.modelo = modelo;
-
-}
-
-String getModelo(){
-
-    return modelo;
-
-}
+    //#region Construtor
+    Carro(){
 
 
+
+    }
+    //#endregion
+
+    //#region Construtor com parâmetros
+    Carro(String cor, String modelo, int capacidadeTanque){
+
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+
+    }
+    //#endregion
+
+    //#region Get e set dos atributos
+    void setCor(String cor){
+
+        this.cor = cor;
+
+    }
+
+    String getCor(){
+
+        return cor;
+
+    }
+
+    void setModelo(String modelo){
+
+        this.modelo = modelo;
+
+    }
+
+    String getModelo(){
+
+        return modelo;
+
+    }
+
+    void setCapacidadeTanque(int capacidadeTanque){
+
+    this.capacidadeTanque = capacidadeTanque;
+
+    }
+
+    int getCapacidadeTanque() {
+
+        return capacidadeTanque;
+
+    }
+    //#endregion
+
+    //#region Método que realiza ação na classe
+    double calcularValor(double vlrGasolina){
+
+        double resultado = vlrGasolina * getCapacidadeTanque();
+        return resultado;
+
+    }
+    //#endregion
 
 }
