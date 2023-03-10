e3d11a1c73a0e4242c6cf316d1302911e6e13bec
Jonathan Franks Oliveira <jonathan.franks011@gmail.com>
2023-03-10 11:39:20 -0300

    #

---
+++ Programação Orientada a Objetos com Java/exercicio-heranca/src/Exercicio_6/Classe1.java
@@ -0,0 +1,27 @@
+package Exercicio_6;
+
+public class Classe1 {
+
+    private String atributo1;
+    public String atributo2;
+    public static String atributo3;
+
+    public void metodo1(){
+
+
+
+    }
+
+    public void metodo2(){
+
+        
+
+    }
+
+    private void metodo3(){
+
+        
+
+    }
+        
+}
